22be86163e9b98a70b930db8009aff02574786cd
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-01 22:00:06 +0300

    Update BH1750_C3-MINI_0.66_Display.ino
    Signed-off-by: Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>

--- BH1750_C3-MINI_0.66_Display/BH1750_C3-MINI_0.66_Display.ino
+++ BH1750_C3-MINI_0.66_Display/BH1750_C3-MINI_0.66_Display.ino
@@ -1,27 +1,20 @@
-#include <stdio.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <BH1750.h>
 
-#define OLED_RESET 7  // GPIO22
+#define OLED_RESET 22 // GPIO22
 Adafruit_SSD1306 display(OLED_RESET);
 
 BH1750 light(0x23);
 
 void setup() {
-
   Serial.begin(115200);
-   Wire.begin(8, 10);
-
+  Wire.begin(8, 10);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
 }
 
 void loop() {
-
-
   // Clear the buffer.
   display.clearDisplay();
   display.setTextSize(1);
@@ -33,13 +26,9 @@ void loop() {
     display.setTextSize(1);
     display.setCursor(40, 21);
     display.print(light.readLightLevel());
-
-  }
-  else
-  {
+  } else {
     display.println("Error!");
   }
   display.display();
   delay(1000);
-
 }
