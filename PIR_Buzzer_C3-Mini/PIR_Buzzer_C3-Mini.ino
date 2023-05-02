f7353ab9d034cb29d46899cf363b8440370b55ac
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-02 15:28:27 +0300

    Update PIR_Buzzer_C3-Mini.ino
    Signed-off-by: Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>

--- PIR_Buzzer_C3-Mini/PIR_Buzzer_C3-Mini.ino
+++ PIR_Buzzer_C3-Mini/PIR_Buzzer_C3-Mini.ino
@@ -2,35 +2,22 @@ bool isToneOn = false;
 int frequency = 500;
 
 void setup() {
-  //here is our PIR sensor
   pinMode(7, INPUT);
-  //here is our buzzer
   pinMode(2, OUTPUT);
-
 }
 
 void loop() {
-  //when PIR sensor gives us HIGH it means that it detects movement
-  if(digitalRead(2) == HIGH){
-    //we will turn on alarm for 15 seconds
-    //we are using tone() so we can control frequency of our beep sound
-    //to turn tone off we have to use noTone()
-    //if you want to change frequency of tone you can do it in the variable
-    //on the top of the code
+  if(digitalRead(7) == HIGH){
     for(int a = 0; a < 30; a++){
       if(isToneOn){
-        noTone(3);  
+        noTone(2);  
         isToneOn = false;  
       }else{
-        //3 means our pin where buzzer is connected
-      tone(3, frequency);
-      //we have to change this variable to true, we have to know
-      //when to turn buzzer on and when to turn it on
-      isToneOn = true;  
+        tone(2, frequency);
+        isToneOn = true;  
       }
-      //delay 0.5 second, you can change this value so it will 
-      //beep slower or faster
       delay(500);
     }
+    noTone(2); // Turn off the buzzer after the loop is finished
   }
 }
