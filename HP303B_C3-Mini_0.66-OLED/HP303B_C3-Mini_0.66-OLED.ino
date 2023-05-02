cee307144b4e409ab0bbbb731a22e1d5c3f66ce1
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-02 15:19:04 +0300

    Update HP303B_C3-Mini_0.66-OLED.ino
    Signed-off-by: Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>

--- HP303B_C3-Mini_0.66-OLED/HP303B_C3-Mini_0.66-OLED.ino
+++ HP303B_C3-Mini_0.66-OLED/HP303B_C3-Mini_0.66-OLED.ino
@@ -5,74 +5,64 @@
 
 LOLIN_HP303B HP303B;
 
+// OLED display setup
 #define OLED_RESET 7
 Adafruit_SSD1306 display(OLED_RESET);
 
-void setup()
-{
-Wire.begin(8,10);
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.setTextColor(WHITE);
-    //Address of the HP303B (0x77 or 0x76)
-    HP303B.begin(); // I2C address = 0x77
-    // HP303B.begin(0x76); //I2C address = 0x76
-    
+void setup() {
+  // I2C setup
+  Wire.begin(8, 10);
+  // OLED display setup
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(WHITE);
+  // HP303B sensor setup
+  HP303B.begin(); // I2C address = 0x77
+  // HP303B.begin(0x76); //I2C address = 0x76
 }
 
-void loop()
-{
-    HP303B.begin();
-    Wire.begin(8,10);
-    // Clear the buffer.
-    display.clearDisplay();
-    display.setCursor(0, 0);
-     
+void loop() {
+  // Clear the display buffer.
+  display.clearDisplay();
+  display.setCursor(0, 0);
 
-    int32_t temperature;
-    int32_t pressure;
-    int16_t oversampling = 7;
-    int16_t ret;
+  int32_t temperature;
+  int32_t pressure;
+  int16_t oversampling = 7;
+  int16_t ret;
 
-    ret = HP303B.measureTempOnce(temperature, oversampling);
-
-    if (ret != 0)
-    {
-        display.setTextSize(1);
-        display.println("T: error");
-    }
-    else
-    {
-        display.setCursor(35,10);
-        display.setTextSize(1);
-        display.println("Temperature:");
-        display.setCursor(40,21);
-        display.setTextSize(1);
-        display.println(temperature);
-         display.display();
-            delay(3000);
-    }
-
-    //create anew display page to display pressure readings
-    ret = HP303B.measurePressureOnce(pressure, oversampling);
-
-    if (ret != 0)
-    {
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.println("P: error");
-    }
-    else
-    {
-        display.clearDisplay();
-        display.setCursor(35,10);
-        display.setTextSize(1);
-        display.println("Pressure:");
-        display.setCursor(40,21);
-        display.setTextSize(1);
-        display.println(pressure);
-         display.display();
-    }
-
+  // Measure temperature
+  ret = HP303B.measureTempOnce(temperature, oversampling);
+  if (ret != 0) {
+    display.setTextSize(1);
+    display.println("T: error");
+  } else {
+    display.setCursor(35,10);
+    display.setTextSize(1);
+    display.println("Temperature:");
+    display.setCursor(40,21);
+    display.setTextSize(1);
+    display.println(temperature);
     display.display();
     delay(3000);
+  }
+
+  // Measure pressure
+  ret = HP303B.measurePressureOnce(pressure, oversampling);
+  if (ret != 0) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.println("P: error");
+  } else {
+    display.clearDisplay();
+    display.setCursor(35,10);
+    display.setTextSize(1);
+    display.println("Pressure:");
+    display.setCursor(40,21);
+    display.setTextSize(1);
+    display.println(pressure);
+    display.display();
+  }
+
+  // Delay before looping again
+  delay(3000);
 }
