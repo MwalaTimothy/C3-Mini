dd7090552452f0b8077271f27a3fc32659ea9c3d
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-10-28 21:47:52 +0300

    Update DHT11_C3-Mini_0.66-OLED.ino
    Signed-off-by: Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>

--- DHT11_C3-Mini_0.66-OLED/DHT11_C3-Mini_0.66-OLED.ino
+++ DHT11_C3-Mini_0.66-OLED/DHT11_C3-Mini_0.66-OLED.ino
@@ -1,4 +1,4 @@
-#include "DHT.h" //DHT Bibliothek
+#include "DHT.h" // DHT Library
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -6,45 +6,44 @@
 const int OLED_RESET = 7;
 Adafruit_SSD1306 display(OLED_RESET);
 
-//Pin an welchem der DHT11 Sensor angeschlossen ist.
-//Beim DHT11 Shield ist es der digitale Pin D4.
+// Pin to which the DHT11 sensor is connected.
+// If using the DHT11 Shield, it's digital Pin D4.
 const int DHTPIN = 6;
 
-//Festlegen welcher Typ von DHT Sensor verwendet wird.
+// Specify the type of DHT sensor being used.
 #define DHTTYPE DHT11
 
-//Initialisieren des Sensors mit dem Anschluss und dem Typ
+// Initialize the sensor with the pin and type.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600); //Begin der seriellen Kommunikation mit 9600 Baud.
-  Wire.begin(8,10);
+  Serial.begin(9600); // Begin serial communication at 9600 Baud.
+  Wire.begin(8, 10);
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
   delay(2000);
   display.clearDisplay(); 
-  dht.begin(); //DHT Kommunikation beginnen.
+  dht.begin(); // Start DHT communication.
 }
 
 void loop() {
-  //Der DHT11 Sensor liefert alle 2 Sekunden einen neuen
-  //Wert daher lohnt es sich nicht die loop konstant durchlaufen 
-  //zu lassen.
+  // The DHT11 sensor provides a new reading every 2 seconds,
+  // so there's no need to constantly loop in the program.
   delay(2000);
 
-  //lesen der Luftfeuchtigkeit
-  double luftfeuchtigkeit = dht.readHumidity();
-  //lesen der Temperatur in Grad Celsius
-  double temperaturC = dht.readTemperature();
-  //lesen der Temperatur in Grad Fahrenheit
-  //mit dem Boolean Parameter wird "gesteuert" ob
-  //die Temperatur in Fahrenheit oder Celsius ausgegeben wird.
-  double temperaturF = dht.readTemperature(true);
+  // Read humidity value.
+  double humidity = dht.readHumidity();
+  // Read temperature in Celsius.
+  double temperatureC = dht.readTemperature();
+  // Read temperature in Fahrenheit.
+  // The boolean parameter controls whether
+  // the temperature is displayed in Fahrenheit or Celsius.
+  double temperatureF = dht.readTemperature(true);
 
-  //Prüfen ob die Werte erfolgreich gelesen wurden.
-  if (isnan(luftfeuchtigkeit) || isnan(temperaturC) || isnan(temperaturF)) {
-    Serial.println("Fehler beim lesen von Daten.");
+  // Check if the values were read successfully.
+  if (isnan(humidity) || isnan(temperatureC) || isnan(temperatureF)) {
+    Serial.println("Error reading data.");
     return;
   }
 
@@ -52,19 +51,19 @@ void loop() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
-  display.setCursor(32,8);
+  display.setCursor(32, 8);
   display.println("DHT11");
 
-  display.setCursor(32,9);
+  display.setCursor(32, 9);
   display.println("_____");
 
-  display.setCursor(32,17);
-  String tempValue = String(temperaturC);
-  display.println("T: "+tempValue+"C");
+  display.setCursor(32, 17);
+  String tempValue = String(temperatureC);
+  display.println("T: " + tempValue + "C");
 
-  display.setCursor(32,25);
-  String humValue = String(luftfeuchtigkeit);
-  display.println("H: "+humValue+"%");
+  display.setCursor(32, 25);
+  String humValue = String(humidity);
+  display.println("H: " + humValue + "%");
 
   display.display();
   delay(500);
